3e85e1b0727499be24e0619d15d8364e916e27c9
Luis Eduardo Rojas <lerl221295@gmail.com>
2019-06-01 18:05:31 -0300

    fix(view) removing unnecesary space at top

--- App.js
+++ App.js
@@ -24,7 +24,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+    backgroundColor: '#fff'
   },
 });
